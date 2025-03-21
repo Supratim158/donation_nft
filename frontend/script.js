6e6b35b48bf1c798d6cc9636659caa4c1eb83110
Supratim Modak <119443063+Supratim158@users.noreply.github.com>
2025-03-21 14:32:04 +0530

    Create script.js

---
+++ frontend/script.js
@@ -0,0 +1,368 @@
+// script.js
+
+const CONTRACT_ADDRESS = "0x3CDeFEE6f524C2d943f1AEe21834C23552BE9271"; // Replace with deployed contract address
+const ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_donationAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_donationPercentage",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "seller",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "NFTListed",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "tokenURI",
+				"type": "string"
+			}
+		],
+		"name": "NFTMinted",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "NFTPurchased",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "buyNFT",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "donationAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "donationPercentage",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "listNFT",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "tokenURI",
+				"type": "string"
+			}
+		],
+		"name": "mintNFT",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "nextTokenId",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "ownerOf",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_donationAddress",
+				"type": "address"
+			}
+		],
+		"name": "setDonationAddress",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_donationPercentage",
+				"type": "uint256"
+			}
+		],
+		"name": "setDonationPercentage",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenPrices",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenSellers",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "tokenURI",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]; // Replace with the ABI of your contract
+
+let web3;
+let contract;
+let accounts;
+
+async function init() {
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        accounts = await web3.eth.getAccounts();
+        contract = new web3.eth.Contract(ABI, CONTRACT_ADDRESS);
+        loadNFTs();
+    } else {
+        alert('Please install MetaMask!');
+    }
+}
+
+async function mintNFT() {
+    const tokenURI = document.getElementById('tokenURI').value;
+    await contract.methods.mintNFT(tokenURI).send({ from: accounts[0] });
+    alert('NFT Minted Successfully!');
+    loadNFTs();
+}
+
+async function listNFT() {
+    const tokenId = document.getElementById('listTokenId').value;
+    const price = web3.utils.toWei(document.getElementById('price').value, 'ether');
+    await contract.methods.listNFT(tokenId, price).send({ from: accounts[0] });
+    alert('NFT Listed Successfully!');
+    loadNFTs();
+}
+
+async function buyNFT() {
+    const tokenId = document.getElementById('buyTokenId').value;
+    const price = await contract.methods.tokenPrices(tokenId).call();
+    await contract.methods.buyNFT(tokenId).send({ from: accounts[0], value: price });
+    alert('NFT Purchased Successfully!');
+    loadNFTs();
+}
+
+async function loadNFTs() {
+    const nfts = document.getElementById('nfts');
+    nfts.innerHTML = '';
+    const totalSupply = await contract.methods.nextTokenId().call();
+    for (let i = 0; i < totalSupply; i++) {
+        try {
+            const owner = await contract.methods.ownerOf(i).call();
+            const tokenURI = await contract.methods.tokenURI(i).call();
+            let price = await contract.methods.tokenPrices(i).call();
+            price = web3.utils.fromWei(price, 'ether');
+
+            const item = document.createElement('li');
+            item.innerHTML = `Token #${i} - Owner: ${owner}<br>URI: ${tokenURI}<br>Price: ${price} ETH`;
+            nfts.appendChild(item);
+        } catch (e) {
+            // Ignore if token doesn't exist or error occurs
+        }
+    }
+}
+
+window.onload = init;
